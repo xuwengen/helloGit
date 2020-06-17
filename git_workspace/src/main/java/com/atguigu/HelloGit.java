e97c06e42031ba2f97797d6b8c5cf488f3090b26
xuwengen <773647038@qq.com>
2020-06-17 20:15:18 +0800

    添加文件

---
+++ git_workspace/src/main/java/com/atguigu/HelloGit.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @author xwg
+ * @date 2020/6/17
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
