715ea9675754035dd91cb908d53590455d502479
xuwengen <773647038@qq.com>
2020-06-17 20:45:18 +0800

    主干添加

--- git_workspace/src/main/java/com/atguigu/HelloGit.java
+++ git_workspace/src/main/java/com/atguigu/HelloGit.java
@@ -10,6 +10,7 @@ public class HelloGit {
         System.out.println("Hello Git");
         System.out.println("Hello git  !");
         System.out.println("Hello git dev !");
-
+        System.out.println("主干添加");
+        System.out.println("分支添加");
     }
 }
